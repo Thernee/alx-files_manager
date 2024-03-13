f4ae80376c61e76ef509a62cc4dc82dc1b9994f1
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-13 12:30:03 +0100

    Fix: lint errors

--- utils/redis.js
+++ utils/redis.js
@@ -8,25 +8,24 @@ class RedisClient {
     this.client.on('error', (err) => console.log(err));
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.connected;
   }
 
-  async get (key) {
+  async get(key) {
     const getKeyFunc = promisify(this.client.get).bind(this.client);
-    return await getKeyFunc(key);
+    return getKeyFunc(key);
   }
 
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     const setKeyFunc = promisify(this.client.set).bind(this.client);
-    return await setKeyFunc(key, value, 'EX', duration);
+    return setKeyFunc(key, value, 'EX', duration);
   }
 
-  async del (key) {
+  async del(key) {
     const delKeyFunc = promisify(this.client.del).bind(this.client);
-    return await delKeyFunc(key);
+    return delKeyFunc(key);
   }
-
 }
 
 const redisClient = new RedisClient();
